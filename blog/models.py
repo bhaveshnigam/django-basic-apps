87241d4f26bed0994c04fceccfbc652efb2f2fd5
Christos Trochalakis <yatiohi@ideopolis.gr>
2008-09-24 13:29:05 +0300

    conform with django 1.0

--- blog/models.py
+++ blog/models.py
@@ -16,16 +16,13 @@ import tagging
 class Category(models.Model):
     """Category model."""
     title       = models.CharField(_('title'), max_length=100)
-    slug        = models.SlugField(_('slug'), prepopulate_from=('title',), unique=True)
+    slug        = models.SlugField(_('slug'), unique=True)
 
     class Meta:
         verbose_name = _('category')
         verbose_name_plural = _('categories')
         db_table = 'blog_categories'
         ordering = ('title',)
-
-    class Admin:
-        pass
 
     def __unicode__(self):
         return u'%s' % self.title
@@ -42,11 +39,11 @@ class Post(models.Model):
         (2, _('Public')),
     )
     title           = models.CharField(_('title'), max_length=200)
-    slug            = models.SlugField(_('slug'), prepopulate_from=('title',))
+    slug            = models.SlugField(_('slug'))
     author          = models.ForeignKey(User, blank=True, null=True)
     body            = models.TextField(_('body'))
     tease           = models.TextField(_('tease'), blank=True)
-    status          = models.IntegerField(_('status'), choices=STATUS_CHOICES, radio_admin=True, default=2)
+    status          = models.IntegerField(_('status'), choices=STATUS_CHOICES, default=2)
     allow_comments  = models.BooleanField(_('allow comments'), default=True)
     publish         = models.DateTimeField(_('publish'))
     created         = models.DateTimeField(_('created'), auto_now_add=True)
@@ -63,11 +60,6 @@ class Post(models.Model):
         db_table  = 'blog_posts'
         ordering  = ('-publish',)
         get_latest_by = 'publish'
-
-    class Admin:
-        list_display  = ('title', 'publish', 'status')
-        list_filter   = ('publish', 'categories', 'status')
-        search_fields = ('title', 'body')
 
     def __unicode__(self):
         return u'%s' % self.title
